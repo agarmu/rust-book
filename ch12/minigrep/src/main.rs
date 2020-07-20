0e47520ed3384d68aed342774fd15c7a6aa7fe86
Mukul <55563106+agarmu@users.noreply.github.com>
2020-07-20 18:48:52 -0500

    Read from file

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
-
+use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
-
+    if args.len() < 3 {
+        return;
+    }
     let query = &args[1];
     let filename = &args[2];
-
     println!("Searching for {}", query);
     println!("In file {}", filename);
+    let contents = fs::read_to_string(filename)
+        .expect("Unable to read file.");
+    println!("With Contents: {}", contents);
 }

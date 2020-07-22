c815ddc4b05746bb14049582ce33d1311935610a
Mukul Agarwal <55563106+agarmu@users.noreply.github.com>
2020-07-21 21:21:15 -0500

    Chapter 12
    * Accept Command Line Arguments
    
    * Read from file
    
    * Refactor for modularity/error handling
    
    * Develop Functionality with test-drive development
    
    * Use environment variables
    
    * Use stderr for error messages via the eprintln! macro

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use minigrep::Config;
+use std::env::args;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem Parsing Arguments: {}", err);
+        exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        exit(1);
+    };
 }

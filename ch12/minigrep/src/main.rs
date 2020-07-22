c881d701f8ed4e2c25620fe2fda963e70c38be23
Mukul <55563106+agarmu@users.noreply.github.com>
2020-07-21 21:16:47 -0500

    Use stderr for error messages via the eprintln! macro

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -5,12 +5,12 @@ use std::process::exit;
 fn main() {
     let args: Vec<String> = args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem Parsing Arguments: {}", err);
+        eprintln!("Problem Parsing Arguments: {}", err);
         exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         exit(1);
     };
 }
